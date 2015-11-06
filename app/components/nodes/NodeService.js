7800d3e1901813a5b77c9e750dfe2b5f44d8b212
Graham Seabrook <graham.seabrook@ag-isolutions.com>
2015-11-06 14:11:53 +0800

    Removed commented lines

--- app/components/nodes/NodeService.js
+++ app/components/nodes/NodeService.js
@@ -51,15 +51,3 @@ function NodeService($http, $q, loginService, config) {
     	})
     }
 }
-
-//NodeListing?_dc=1446783517577
-//&anode=&start=0&limit=20
-//&json={"ebikko_session_id":"d86d6442d9ae44a4a0a990b3903a4ee5","selected_columns":[22,12,1,4,10,4.1],
-//"method":"SEARCH","saved_search_id":"ia4065fe384245cc85e0670b7bb10c15",
-//"selected_saved_search_id":"ia4065fe384245cc85e0670b7bb10c15",
-//"search_id":"ia4065fe384245cc85e0670b7bb10c15"}
-//&cache_id=45cfcffe-e658-4308-3ccf-cd63634477b9
-
-//http://localhost:8080/ebikkoweb/
-//NodeListing?json={"ebikko_session_id":"gd9d2baef01f452e8bde27ed99607260","selected_columns":[22,12,1,4,10,4.1],"method":"SEARCH"}&limit=25&start=0
-
