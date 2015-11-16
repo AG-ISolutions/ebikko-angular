f11644bbd6861abecf4bad73b224858c8d95eefd
Graham Seabrook <graham.seabrook@ag-isolutions.com>
2015-11-16 15:09:49 +0800

    Added loading spinner

--- app/components/nodes/NodesController.js
+++ app/components/nodes/NodesController.js
@@ -31,7 +31,12 @@
                 return 'tree_node';
             },
             options: {
-                clickableNodeNames: true
+                clickableNodeNames: true,
+                expanderTemplate: 
+                    "<svg class='spinner' width='20px' height='20px' viewBox='0 0 66 66' xmlns='http://www.w3.org/2000/svg'>" +
+                    "<circle class='path' fill='none' stroke-width='6' stroke-linecap='round' cx='33' cy='33' r='30'></circle>" +
+                    "</svg>" +
+                    "<a href='#'>&nbsp;</a>"
             }
         });
 
@@ -43,8 +48,7 @@
             if (node._is_leaf) {
                 tabService.addTab({
                     name: node.title,
-                    content: 
-                        "<iframe src='" + nodeService.getContentUrl(node.node_id) + "'/>'"
+                    content: "<iframe src='" + nodeService.getContentUrl(node.node_id) + "'/>'"
                 });
             }
         }
